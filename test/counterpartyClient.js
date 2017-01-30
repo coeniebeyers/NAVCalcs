abaaf6ec02d94d07353a3149fd128bec9714467e
Coenie Beyers <coeniebeyers@gmail.com>
2017-01-30 16:10:54 +0200

    Removed console log from test

--- test/counterpartyClient.js
+++ test/counterpartyClient.js
@@ -5,7 +5,7 @@ describe('counterpartyClient', function() {
   this.timeout(60000);
   it('should be able to get balances of token holders', function(done) {
     counterpartyClient.GetBalances(function(balances){
-      console.log('balances:', balances);
+      //console.log('balances:', balances);
       expect(balances).to.not.be(null);
       expect(balances.data).to.not.be(null);
       expect(balances.data.length).to.be.greaterThan(0);
